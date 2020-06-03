78007acbd52b52eb911f095b57db682ea7b927a9
Freddie Hoy <freddiehoy@hotmail.com>
2020-06-03 09:48:17 +0100

    Set up with basic express backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+const pageOne = {
+  path: '/page-a',
+  linkTitle: 'Page A',
+  pageTitle: 'Some Page',
+  pageDescription: 'ipsum dolor sit amet...',
+  table: {
+    apiEndpoint: '/page-a-data.json',
+    columns: [{ title: 'Name' }, { title: 'Email' }, { title: 'Age' }]
+  }
+}
+
+// create a GET route
+app.get('/express_backend/page1', (req, res) => {
+  res.send({ body: pageOne })
+})

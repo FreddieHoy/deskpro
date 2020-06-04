45cca4ec80a5c49e4d71ad2f364932e067a8efcb
Freddie Hoy <freddiehoy@hotmail.com>
2020-06-04 14:53:16 +0100

    Reformat api data

--- api/server.js
+++ api/server.js
@@ -1,25 +1,28 @@
-const serverData = require('./serverData.js')
+const appData = require('./serverData.js').appData
+const tableOneData = require('./serverData.js').tableOneData
+const tableTwoData = require('./serverData.js').tableTwoData
+const tableThreeData = require('./serverData.js').tableThreeData
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
-// create a GET routelink
+// Routes
 app.get('/express_backend/appData', (req, res) => {
-  res.send({ body: serverData.appData })
+  res.send({ body: appData })
 })
 
 app.get('/express_backend/tables/table-one', (req, res) => {
-  res.send({ body: serverData.tableOneData })
+  res.send({ body: tableOneData })
 })
 
 app.get('/express_backend/tables/table-two', (req, res) => {
-  res.send({ body: serverData.tableTwoData })
+  res.send({ body: tableTwoData })
 })
 
 app.get('/express_backend/tables/table-three', (req, res) => {
-  res.send({ body: serverData.tableThreeData })
+  res.send({ body: tableThreeData })
 })
--- api/serverData.js
+++ api/serverData.js
@@ -1,104 +1,122 @@
-const serverData = {
-  appData: [
-    {
-      path: '/page-one',
-      linkTitle: 'Page One',
-      pageTitle: 'Some Page One',
-      pageDescription:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-      table: {
-        apiEndpoint: 'table-one',
-        columns: [
-          { title: 'Name' },
-          { title: 'Email' },
-          { title: 'Age' },
-          { title: 'Height' }
-        ]
-      }
-    },
-    {
-      path: '/page-two',
-      linkTitle: 'Page Two',
-      pageTitle: 'Some Page Two',
-      pageDescription:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-      table: {
-        apiEndpoint: 'table-two',
-        columns: [
-          { title: 'Name' },
-          { title: 'Email' },
-          { title: 'Age' },
-          { title: 'Height' },
-          { title: 'Address' }
-        ]
-      }
-    },
-    {
-      path: '/page-three',
-      linkTitle: 'Page Three',
-      pageTitle: 'Some Page Three',
-      pageDescription:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-      table: {
-        apiEndpoint: 'table-three',
-        columns: [
-          { title: 'Name' },
-          { title: 'Email' },
-          { title: 'Age' },
-          { title: 'Height' },
-          { title: 'Profession' }
-        ]
-      }
+const appData = [
+  {
+    path: '/page-one',
+    linkTitle: 'Page One',
+    pageTitle: 'Some Page One',
+    pageDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    table: {
+      apiEndpoint: 'table-one',
+      columns: [
+        { title: 'Name' },
+        { title: 'Email' },
+        { title: 'Age' },
+        { title: 'Height' }
+      ]
     }
-  ],
-  tableOneData: [
-    {
-      name: 'Fred Hoy',
-      email: 'freddiehoy0@gamil.com',
-      age: 25,
-      height: 6.3
-    },
-    {
-      name: 'Jeff',
-      email: 'jeff@notarealemail.com',
-      age: 30,
-      height: 6
+  },
+  {
+    path: '/page-two',
+    linkTitle: 'Page Two',
+    pageTitle: 'Some Page Two',
+    pageDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    table: {
+      apiEndpoint: 'table-two',
+      columns: [
+        { title: 'Name' },
+        { title: 'Email' },
+        { title: 'Age' },
+        { title: 'Height' },
+        { title: 'Address' }
+      ]
     }
-  ],
-  tableTwoData: [
-    {
-      name: 'Fred Hoy',
-      email: 'freddiehoy0@gamil.com',
-      age: 25,
-      height: 6.3,
-      address: '10 Long Road'
-    },
-    {
-      name: 'Jeff',
-      email: 'jeff@notarealemail.com',
-      age: 30,
-      height: 6,
-      address: '20 Small Avenue'
+  },
+  {
+    path: '/page-three',
+    linkTitle: 'Page Three',
+    pageTitle: 'Some Page Three',
+    pageDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    table: {
+      apiEndpoint: 'table-three',
+      columns: [
+        { title: 'Name' },
+        { title: 'Email' },
+        { title: 'Age' },
+        { title: 'Height' },
+        { title: 'Profession' }
+      ]
     }
-  ],
-  tableThreeData: [
-    {
-      name: 'Fred Hoy',
-      email: 'freddiehoy0@gamil.com',
-      age: 25,
-      height: 6.3,
-      profession: 'Software Engineer'
-    },
-    {
-      name: 'Jeff',
-      email: 'jeff@notarealemail.com',
-      age: 30,
-      height: 6,
-      profession: 'pro swimmer'
-    }
-  ]
+  }
+]
+
+const tableOneData = [
+  {
+    name: 'Fred Hoy',
+    email: 'freddiehoy0@gamil.com',
+    age: 25,
+    height: 6.3
+  },
+  {
+    name: 'Jeff',
+    email: 'jeff@notarealemail.com',
+    age: 30,
+    height: 6
+  }
+]
+
+const tableTwoData = [
+  {
+    name: 'Fred Hoy',
+    email: 'freddiehoy0@gamil.com',
+    age: 25,
+    height: 6.3,
+    address: '10 Long Road'
+  },
+  {
+    name: 'Jeff',
+    email: 'jeff@notarealemail.com',
+    age: 30,
+    height: 6,
+    address: '20 Small Avenue'
+  },
+  {
+    name: 'Anna',
+    email: 'anna@notarealemail.com',
+    age: 28,
+    height: 6,
+    address: '30 Round Street'
+  },
+  {
+    name: 'Gregory',
+    email: 'greg@notarealemail.com',
+    age: 21,
+    height: 7,
+    address: '40 Tall Mews'
+  }
+]
+
+const tableThreeData = [
+  {
+    name: 'Fred Hoy',
+    email: 'freddiehoy0@gamil.com',
+    age: 25,
+    height: 6.3,
+    profession: 'Software Engineer'
+  },
+  {
+    name: 'Jeff',
+    email: 'jeff@notarealemail.com',
+    age: 30,
+    height: 6,
+    profession: 'pro swimmer'
+  }
+]
+
+module.exports = {
+  appData: appData,
+  tableOneData: tableOneData,
+  tableTwoData: tableTwoData,
+  tableThreeData: tableThreeData
 }
-
-module.exports = serverData
-
-// ES5 named exports

c6000bd37ba412a4eb2b124c9bcc311d2dd887fb
Freddie Hoy <freddiehoy@hotmail.com>
2020-06-04 12:58:09 +0100

    Make Icon example cell component

---
+++ src/components/exampleCells/IconCell.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IconCellProps {
+  alt: string
+  imageSrc: string
+}
+
+const IconCell = (props: IconCellProps) => {
+  const { alt, imageSrc } = props
+  return <img src={imageSrc} alt={alt} />
+}
+
+export default IconCell
